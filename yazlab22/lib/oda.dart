8d1b69f0d5a5650f3da8d40afc7cde11bdea1d64
ebrurazi <117021777+ebrurazi@users.noreply.github.com>
2024-07-28 15:58:59 +0300

    Update oda.dart

--- yazlab22/lib/oda.dart
+++ yazlab22/lib/oda.dart
@@ -29,7 +29,7 @@ class _OdaState extends State<Oda> {
     super.initState();
     final FirebaseDatabase database = FirebaseDatabase(
         databaseURL:
-            'https://flutter-firebase-6ce5a-default-rtdb.europe-west1.firebasedatabase.app/');
+            '**********');
     _dbRef = database.ref().child('odalar');
     addUserToRoomFromFirestore();
     getGameRequest();
